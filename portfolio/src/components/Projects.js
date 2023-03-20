4df9e228c3f73c6e47f2a94bbcb7f2693b4dfc66
Nicholas Chikuji <chiku524@icloud.com>
2023-03-20 16:56:51 -0400

    fixed up projects page

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, { useState, useLayoutEffect, useEffect } from "react";
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ExternalLink } from "react-external-link";
@@ -11,18 +11,33 @@ import '../css/projects.css';
 
 const Projects = () => {
 
+    useLayoutEffect(() => {
+        gsap.set(".cards-container .projects-title div h1", {scale: 0, opacity: 0, y: 0})
+
+        gsap.to(`.cards-container .projects-title div h1`, {
+            scale: 1,
+            opacity: 1, 
+            color: "#00a4d6",
+            y: -45,
+            ease: "ease",
+            duration: 0.15,
+            transformOrigin: "50% 50%",
+            stagger: {from: "edges", amount: 0.7},
+            textShadow: "0px 0px 8px rgb(32, 32, 32)"
+        })
+    }, [])
+    
     //anim project border
   useLayoutEffect(() => {
     let animBorder = gsap.timeline();
-
     animBorder
-    .from(".project", {
+    .from(".cards-container", {
         boxShadow: "0px 0px 10px #424141",
         borderRadius: '10%',
         ease: "elastic",
         yoyo: true
     })
-    .to(".project", {
+    .to(".cards-container", {
       keyframes: {
         boxShadow: ["0px 0px 10px #424141", "0px 0px 15px #e2bd1a", "0px 0px 20px #29f176", "0px 0px 20px rgb(41, 128, 241)", "0px 0px 15px #c418bb", "0px 0px 10px #424141"],
       },
@@ -31,60 +46,78 @@ const Projects = () => {
     })
   }, [])
 
+  useEffect(() => {
+    let expat = document.querySelector(".project-link.expat");
+    let hajjar = document.querySelector(".project-link.hajjar");
+    let xtrades = document.querySelector(".project-link.xtrades");
+    let metaverse = document.querySelector(".project-link.metaverse");
+    let animations = document.querySelector(".project-link.animations");
+    let expatImg = document.querySelector(".expat-img");
+    let hajjarImg = document.querySelector(".hajjar-img");
+    let xtradesImg = document.querySelector(".xtrades-img");
+    let metaverseImg = document.querySelector(".metaverse-img");
+    let animationsImg = document.querySelector(".animations-img");
+
+    expat.addEventListener("mouseover", () => {gsap.to(expatImg, {zIndex: 1, display: "block", scale: 1})})
+    expat.addEventListener("mouseleave", () => {gsap.to(expatImg, {})})
+
+    hajjar.addEventListener("mouseover", () => {gsap.to(hajjarImg, {zIndex: 1, display: "block", scale: 1})})
+    hajjar.addEventListener("mouseleave", () => {gsap.to(hajjarImg, {display: "none"})})
+
+    xtrades.addEventListener("mouseover", () => {gsap.to(xtradesImg, {zIndex: 1, display: "block", scale: 1})})
+    xtrades.addEventListener("mouseleave", () => {gsap.to(xtradesImg, {display: "none"})})
+
+    metaverse.addEventListener("mouseover", () => {gsap.to(metaverseImg, {zIndex: 1, display: "block", scale: 1})})
+    metaverse.addEventListener("mouseleave", () => {gsap.to(metaverseImg, {display: "none"})})
+
+    animations.addEventListener("mouseover", () => {gsap.to(animationsImg, {zIndex: 1, display: "block", scale: 1})})
+    animations.addEventListener("mouseleave", () => {gsap.to(animationsImg, {display: "none"})})
+    
+  }, [])
+    
+
     return(
         <div className="cards-container">
-            <ExternalLink href="https://expatjournal3.netlify.app/" className="project-link">
-                <div className="project project-1">  
-                    <img src={ExpatJournal} />  
-                    <span>Expat Journal</span>
-                    <p>This is a project I worked on with a group at the coding bootcamp I partook in. We were to build a webapp that allows expats to connect to each other and share their travelling stories.</p>
-                </div>
-            </ExternalLink>
-            <ExternalLink href="https://hajjar-associates-remake.vercel.app/" className="project-link">
-                <div className="project project-1">  
-                    <img src={HajjarAssociatesRemake} />  
-                    <span>Hajjar Associates Remake</span>
-                    <p>Here is a website I created for a friend of mine's company. It was left unfinished but is a demonstration of what I am capable of achieving with React.</p>
-                </div>
-            </ExternalLink>
-            <ExternalLink href="https://xtrades-challenge.vercel.app/" className="project-link">
-                <div className="project project-1">  
-                    <img src={Xtrades} />  
-                    <span>X-trades 24 hour challenge</span>
-                    <p>In this project, I was to create a stock exchange UI within 24 hours to the best of my ability. It was an interview coding test that shows what I am capable of doing in a fast paced environment.</p>
-                </div>
-            </ExternalLink>
-            <ExternalLink href="https://metaverse-chiku524.vercel.app/" className="project-link">
-                <div className="project project-1">  
-                    <img src={Metaverse} />  
-                    <span>Metaverse</span>
-                    <p>This is a freelance project I started for a gaming group of people who wanted to build a metaverse. They abandoned the project as I was working on it but it was great experience nonetheless.</p>
-                </div>
-            </ExternalLink>
-            <ExternalLink href="https://portfolio-site-chiku524.vercel.app/" className="project-link">
-                <div className="project project-1">  
-                    <img src={AnimationsSite} />  
-                    <span>Animations Site</span>
-                </div>
-            </ExternalLink>
-            <ExternalLink href="https://expatjournal3.netlify.app/" className="project-link">
-                <div className="project project-1">  
-                    <img src={ExpatJournal} />  
-                    <span>Expat Journal</span>
-                </div>
-            </ExternalLink>
-            <ExternalLink href="https://expatjournal3.netlify.app/" className="project-link">
-                <div className="project project-1">  
-                    <img src={ExpatJournal} />  
-                    <span>Expat Journal</span>
-                </div>
-            </ExternalLink>
-            <ExternalLink href="https://expatjournal3.netlify.app/" className="project-link">
-                <div className="project project-1">  
-                    <img src={ExpatJournal} />  
-                    <span>Expat Journal</span>
-                </div>
-            </ExternalLink>
+            <div className='projects-title'>
+                    <div><h1>H</h1><h1>E</h1><h1>R</h1><h1>E</h1><h1>&nbsp;</h1><h1>A</h1><h1>R</h1><h1>E</h1></div>
+                    <div><h1>&nbsp;</h1><h1>S</h1><h1>O</h1><h1>M</h1><h1>E</h1><h1>&nbsp;</h1></div>
+                    <div><h1>O</h1><h1>F</h1><h1>&nbsp;</h1><h1>M</h1><h1>Y</h1><h1>&nbsp;</h1><h1>S</h1><h1>I</h1><h1>D</h1><h1>E</h1></div>
+                    <div><h1>&nbsp;</h1><h1>P</h1><h1>R</h1><h1>O</h1><h1>J</h1><h1>E</h1><h1>C</h1><h1>T</h1><h1>S</h1></div>
+            </div>
+            <div className="project-imgs">
+                <img src={ExpatJournal} className="expat-img"/> 
+                <img src={HajjarAssociatesRemake} className="hajjar-img"/> 
+                <img src={Xtrades} className="xtrades-img"/> 
+                <img src={Metaverse} className="metaverse-img"/>
+                <img src={AnimationsSite} className="animations-img"/> 
+            </div>
+            <div className="projects">
+                <ExternalLink href="https://expatjournal3.netlify.app/" className="project-link expat">
+                    <div className="project project-1">  
+                        <span className="expat">Expat Journal</span>
+                    </div>
+                </ExternalLink>
+                <ExternalLink href="https://hajjar-associates-remake.vercel.app/" className="project-link hajjar">
+                    <div className="project project-1">  
+                        <span className="hajjar">Hajjar Associates Remake</span>
+                    </div>
+                </ExternalLink>
+                <ExternalLink href="https://xtrades-challenge.vercel.app/" className="project-link xtrades">
+                    <div className="project project-1">  
+                        <span className="xtrades">X-trades 24 hour challenge</span>
+                    </div>
+                </ExternalLink>
+                <ExternalLink href="https://metaverse-chiku524.vercel.app/" className="project-link metaverse">
+                    <div className="project project-1">  
+                        <span className="metaverse">Metaverse</span>
+                    </div>
+                </ExternalLink>
+                <ExternalLink href="https://portfolio-site-chiku524.vercel.app/" className="project-link animations">
+                    <div className="project project-1">  
+                        <span className="animations">Animations Site</span>
+                    </div>
+                </ExternalLink>
+            </div>
         </div>
     )
 }
